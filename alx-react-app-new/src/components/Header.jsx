f976585323bee151e764094d5f0e3fd776522582
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-16 11:18:40 +0100

    Added alx-react-app-new with inline styled components

---
+++ alx-react-app-new/src/components/Header.jsx
@@ -0,0 +1,11 @@
+<header style={{backgroundColor: 'navy', color:'white', textAlign:'center'}}>
+<h1>My favourite Cities</h1>
+</header>
+function Header() {
+    return (
+        <header>
+            <h1>My favourite cities</h1>
+        </header>
+    );
+}
+export default Header;
