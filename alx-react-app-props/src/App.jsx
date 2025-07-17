ab2772e9a7c74ff461a8b5b05259f173ff0a34a9
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-17 16:40:05 +0100

    update all content

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,60 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
+import UserProfile from './components/UserProfile';
+import Counter from './components/Counter'
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <WelcomeMessage />
+    <Header />
+      <MainContent />
+        <Footer />
+        <UserProfile 
+  name="Tolulope" 
+  age="22" 
+  bio="Loves reading and photography" 
+/>
+
+<div>
+  <h1 style={{ textAlign: 'center' }}>My React App</h1>
+      <Counter />
+</div>
+
+      <div>
+
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+  
+}
+
+
+export default App
---
+++ alx-react-app-props/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import UserProfile from './UserProfile';
+function MainContent() {
+    return (
+        <main style={{ padding: '20px', backgroundColor: '#f0f8ff', color: '#333', fontSize: '18px', textAlign: 'center' }} >
+            <p>"I love to visit New York, Paris, and Tokyo."</p>
+        </main>
+    );
+}
+export default MainContent;
