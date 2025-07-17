6c0199aeb636f01e0e56c0eb560d2e6afd8bd82c
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-17 16:08:14 +0100

    update all content

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
-
+import Counter from './components/Counter'
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -19,12 +20,18 @@ function App() {
       <MainContent />
         <Footer />
         <UserProfile 
-  name="Alice" 
-  age="25" 
-  bio="Loves hiking and photography" 
+  name="Tolulope" 
+  age="22" 
+  bio="Loves reading and photography" 
 />
 
+<div>
+  <h1 style={{ textAlign: 'center' }}>My React App</h1>
+      <Counter />
+</div>
+
       <div>
+
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
