797f762fd86f18e43794f9206f301b1fb45f1d3f
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-17 18:50:38 +0100

    update all content

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import Navbar from './Navbar';
 import Home from './pages/Home';
 import About from './pages/About';
 import Services from './pages/Services';
