9204b4a7983d0bff6f9d85e9bd90a5c2542bb5cb
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-17 15:32:09 +0100

    update all content

--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,6 +1,8 @@
+import React from 'react'
+import UserProfile from './UserProfile';
 function MainContent() {
     return (
-        <main>
+        <main style={{ padding: '20px', backgroundColor: '#f0f8ff', color: '#333', fontSize: '18px', textAlign: 'center' }} >
             <p>I love to visit New York paris and Tokyo</p>
         </main>
     );
