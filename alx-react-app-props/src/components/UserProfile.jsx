83bb72b6d806f64ce53eba8c76df7ea4a5faad1e
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-17 17:10:01 +0100

    update all content

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import UserContext from './UserContext';
 
 function UserProfile(props) {
+const user = useContext(UserContext);
+
   return (
     <div style={{ border: '1px solid gray', padding: '10px', margin: '10px'}}>
-      <h2 style={{ color: 'blue' }}>{props.name}</h2>
-
+      <h2 style={{ color: 'blue' }}>{props.name}User Profile</h2>
+       <p><strong>Name:</strong> {user.name}</p>
+      <p><strong>Email:</strong> {user.email}</p>
       <p>Age: <span style={{ fontWeight:'bold'}}>{props.age}</span></p>
       <p>Bio: {props.bio}</p>
     </div>
