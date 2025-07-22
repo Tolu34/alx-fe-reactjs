74720709e6ad6f2ac5ba86600cd51ace7a316450
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-22 22:27:33 +0100

    update

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,18 @@
-import  userecipeStore  from '../store/recipeStore';
+import userecipeStore from '../store/recipeStore';
 
-  const RecipeList = () => {
-    const recipes = userecipeStore(state => state.recipes);
+const RecipeList = () => {
+  const recipes = userecipeStore(state => state.recipes);
 
-    return (
-      <div>
-        {recipes.map(recipe => (
-          <div key={recipe.id}>
-            <h3>{recipe.title}</h3>
-            <p>{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    );
-  };
+  return (
+    <div>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
 export default RecipeList;
-  
