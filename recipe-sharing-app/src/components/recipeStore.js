f34adb7f6ce25f62baf6ec3a5b6d8c2fe41abd6c
Tech Girly <rebeccatolulope2@gmail.com>
2025-07-24 13:32:37 +0100

    update

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,8 +1,8 @@
 import  { create } from 'zustand'
 
-const userecipeStore = create(set => ({
+const useRecipeStore = create(set => ({
   recipes: [],
   addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
   setRecipes: (recipes) => set({ recipes })
 }));
-export default userecipeStore;
+export default useRecipeStore;
