8b739d8ea725a05e7c87a6605c2817201eb44543
Tech Girly <rebeccatolulope2@gmail.com>
2025-08-15 14:45:47 +0100

    update content

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,13 +1,20 @@
-function UserProfile() {
+  function UserProfile() {
   return (
-    <div className="bg-gray-100 p-8 w-full max-w-sm mx-auto my-20 rounded-lg shadow-lg text-center">
+    <div className="flex items-center justify-center min-h-screen bg-gray-100 p-4 sm:p-4 md:p-8">
+        <div className="bg-white shadow-lg rounded-lg flex flex-col items-center text-center
+        p-4 sm:p-4 md:p-8 max-w-xs sm:max-w-xs md:max-wsm w-full ">
+
+        
       <img 
       src="https://via.placeholder.com/150" 
-      alt="User" 
-      className="rounded-full w-36 h-36 mx-auto"/>
-      <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
-      <p className="text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
+      alt="Profile" 
+      className="rounded-full object-cover w-24 h-24 sm:h-24 md:w-36 mb-4"/>
+      <h1 className=" font-bold  text-lg sm:text-lg md:text-xl text-blue-700">John Doe</h1>
+      <p className="text-gray-600 mt-2 text-sm sm:text-sm md:tetx-base">
+        Developer at Example Co. Loves to write code and explore new technologies.</p>
+        </div>
     </div>
+    
   );
 }
 
