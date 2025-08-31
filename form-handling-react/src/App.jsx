1ec282493d4bb7ddb776c7c9405c8ec2f5840ab0
Tech Girly <rebeccatolulope2@gmail.com>
2025-08-31 20:48:46 +0100

    update

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/formikForm";
+import FormikForm from "./components/FormikForm";
 
 function App() {
   return (
